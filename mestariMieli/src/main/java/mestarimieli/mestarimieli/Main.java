31f491fc63ab9635ebc1aa3937710f0b0bc1254e
lzkosone <lea.kosonen@helsinki.fi>
2016-12-31 20:13:01 +0200

    uudenvuoden uusia tuulia kuten mainin pilkkomista

--- mestariMieli/src/main/java/mestarimieli/mestarimieli/Main.java
+++ mestariMieli/src/main/java/mestarimieli/mestarimieli/Main.java
@@ -23,66 +23,72 @@ nelikirjaimisen jonon, joka muodostuu numeroista 123456789, 0 ei ole käytössä
  */
 public class Main {
 
-    static int blacks, whites, guesses;
-    static String answer, guess;
-    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//    static int blacks, whites, guesses;
+//    static String answer, guess;
+//    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws Exception {
-        answer = generate(shuffle("123456789"));
-        while (true) {
-
-            guesses++;
-            echo(answer + "Type number: ");
-            guess = input.readLine();
-
-            if (guess.length() == 4) {
-                for (int i = 0; i <= 3; i++) {
-                    if (guess.charAt(i) == answer.charAt(i)) {
-                        blacks++;
-                    } else if (answer.contains(guess.charAt(i) + "")) {
-                        whites++;
-                    }
-                }
-
-                if (blacks == 4) {
-                    echo("You win in " + guesses + " attempts" + "\n");
-                } else {
-                    echo("Blacks " + blacks + " Whites " + whites + "\n");
-                }
-//            echo("Bulls "+  bulls + " Cows " + cows + "\n");
-                blacks = 0;
-                whites = 0;
-            } else {
-                echo("#### number format!");
-            }
-        }
+        
+        Mestarimieli m = new  Mestarimieli();
+        
+        m.run();
+        
     }
+//        answer = generate(shuffle("123456789"));
+//        while (true) {
+//
+//            guesses++;
+//            echo(answer + "Type number: ");
+//            guess = input.readLine();
+//
+//            if (guess.length() == 4) {
+//                for (int i = 0; i <= 3; i++) {
+//                    if (guess.charAt(i) == answer.charAt(i)) {
+//                        blacks++;
+//                    } else if (answer.contains(guess.charAt(i) + "")) {
+//                        whites++;
+//                    }
+//                }
+//
+//                if (blacks == 4) {
+//                    echo("You win in " + guesses + " attempts" + "\n");
+//                } else {
+//                    echo("Blacks " + blacks + " Whites " + whites + "\n");
+//                }
+////            echo("Bulls "+  bulls + " Cows " + cows + "\n");
+//                blacks = 0;
+//                whites = 0;
+//            } else {
+//                echo("#### number format!");
+//            }
+//        }
+//    }
 
-    public static void echo(String text) {
-        System.out.print(text);
-    }
-
-    public static String generate(String sh) {
-        StringBuilder answ = new StringBuilder();
-        for (int i = 0; i <= 3; i++) {
-            answ.append(sh.charAt(i));
-        }
-        return answ.toString();
-
-    }
-
-    public static String shuffle(String input) {
-        List<Character> characters = new ArrayList<Character>();
-        for (char c : input.toCharArray()) {
-            characters.add(c);
-        }
-        StringBuilder output = new StringBuilder(input.length());
-        while (characters.size() != 0) {
-            int randPicker = (int) (Math.random() * characters.size());
-            output.append(characters.remove(randPicker));
-        }
-        return output.toString();
-
-    }
+//    public static void echo(String text) {
+//        System.out.print(text);
+//    }
+//
+//    public static String generate(String sh) {
+//        StringBuilder answ = new StringBuilder();
+//        for (int i = 0; i <= 3; i++) {
+//            answ.append(sh.charAt(i));
+//        }
+//        return answ.toString();
+//
+//    }
+//
+//    public static String shuffle(String input) {
+//        List<Character> characters = new ArrayList<Character>();
+//        for (char c : input.toCharArray()) {
+//            characters.add(c);
+//        }
+//        StringBuilder output = new StringBuilder(input.length());
+//        while (characters.size() != 0) {
+//            int randPicker = (int) (Math.random() * characters.size());
+//            output.append(characters.remove(randPicker));
+//        }
+//        return output.toString();
+//
+//    }
 
 }
--- mestariMieli/src/main/java/mestarimieli/mestarimieli/Mestarimieli.java
+++ mestariMieli/src/main/java/mestarimieli/mestarimieli/Mestarimieli.java
@@ -5,10 +5,157 @@
  */
 package mestarimieli.mestarimieli;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author lea
  */
 public class Mestarimieli {
-    
+
+    int blacks, whites, guesses;
+    String answer, guess;
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    public void run() throws Exception {
+        //    public static void echo(String text) {
+        //        System.out.print(text);
+        //    }
+        //
+        //    public static String generate(String sh) {
+        //        StringBuilder answ = new StringBuilder();
+        //        for (int i = 0; i <= 3; i++) {
+        //            answ.append(sh.charAt(i));
+        //        }o
+        //        return answ.toString();
+        //
+        //    }
+        //
+        //    public static String shuffle(String input) {
+        //        List<Character> characters = new ArrayList<Character>();
+        //        for (char c : input.toCharArray()) {
+        //            characters.add(c);
+        //        }
+        //        StringBuilder output = new StringBuilder(input.length());
+        //        while (characters.size() != 0) {
+        //            int randPicker = (int) (Math.random() * characters.size());
+        //            output.append(characters.remove(randPicker));
+        //        }
+        //        return output.toString();
+        //
+        //    }
+
+//    private boolean checkAnswer() {
+//
+//        if (guess.length() == 4) {
+//            for (int i = 0; i <= 3; i++) {
+//                if (guess.charAt(i) == answer.charAt(i)) {
+//                    blacks++;
+//                } else if (answer.contains(guess.charAt(i) + "")) {
+//                    whites++;
+//                }
+//            }
+//
+//            if (blacks == 4) {
+//                echo("You win in " + guesses + " attempts" + "\n");
+//            } else {
+//                echo("Blacks " + blacks + " Whites " + whites + "\n");
+//            }
+////            echo("Bulls "+  bulls + " Cows " + cows + "\n");
+//            blacks = 0;
+//            whites = 0;
+//        } else {
+//            echo("#### number format!");
+//        }
+//
+//    }
+        answer = generate(shuffle("123456789"));
+
+        while (true) {
+
+            guesses++;
+            echo(answer + "Type number: ");
+            guess = input.readLine();
+
+            checkAnswer();
+
+//        if (guess.length() == 4) {
+//            for (int i = 0; i <= 3; i++) {
+//                if (guess.charAt(i) == answer.charAt(i)) {
+//                    blacks++;
+//                } else if (answer.contains(guess.charAt(i) + "")) {
+//                    whites++;
+//                }
+//            }
+//
+//            if (blacks == 4) {
+//                echo("You win in " + guesses + " attempts" + "\n");
+//            } else {
+//                echo("Blacks " + blacks + " Whites " + whites + "\n");
+//            }
+////            echo("Bulls "+  bulls + " Cows " + cows + "\n");
+//            blacks = 0;
+//            whites = 0;
+//        } else {
+//            echo("#### number format!");
+//        }
+        }
+
+    }
+
+    private void checkAnswer() {
+
+        if (guess.length() == 4) {
+            for (int i = 0; i <= 3; i++) {
+                if (guess.charAt(i) == answer.charAt(i)) {
+                    blacks++;
+                } else if (answer.contains(guess.charAt(i) + "")) {
+                    whites++;
+                }
+            }
+
+            if (blacks == 4) {
+                echo("You win in " + guesses + " attempts" + "\n");
+            } else {
+                echo("Blacks " + blacks + " Whites " + whites + "\n");
+            }
+//            echo("Bulls "+  bulls + " Cows " + cows + "\n");
+            blacks = 0;
+            whites = 0;
+        } else {
+            echo("#### number format!");
+        }
+
+    }
+
+    public void echo(String text) {
+        System.out.print(text);
+    }
+
+    public String generate(String sh) {
+        StringBuilder answ = new StringBuilder();
+        for (int i = 0; i <= 3; i++) {
+            answ.append(sh.charAt(i));
+        }
+        return answ.toString();
+
+    }
+
+    public String shuffle(String input) {
+        List<Character> characters = new ArrayList<Character>();
+        for (char c : input.toCharArray()) {
+            characters.add(c);
+        }
+        StringBuilder output = new StringBuilder(input.length());
+        while (characters.size() != 0) {
+            int randPicker = (int) (Math.random() * characters.size());
+            output.append(characters.remove(randPicker));
+        }
+        return output.toString();
+
+    }
+
 }
